493c6c836fa6fab210dc5aadca76a76044f072ef
David <davidfarinamorena.guadalupe@alumnado.fundacionloyola.net>
2025-02-11 13:32:50 +0100

    Datos Vacios

--- vistas/InsertarAmbito.php
+++ vistas/InsertarAmbito.php
@@ -8,15 +8,23 @@
 </head>
 <body>
     <?php
-    if (!empty($_POST['ambitos'])) {
-        $nombreAmbitos = $_POST['ambitos'];
-        require_once('../controladores/Cambito.php');
-        $objambito = new Cambito();
-        $resultado = $objambito->cInsertarAmbitos($nombreAmbitos);
-        echo $resultado;
-    } else {
-        echo "Error en la Inserción";
-    }
+        $sw=0;
+        foreach($_POST['ambitos'] as $existeAmbito){
+            if(empty($existeAmbito)){
+                $sw=1;
+            }
+        }
+        if ($sw==0) {
+            $nombreAmbitos = $_POST['ambitos'];
+            require_once('../controladores/Cambito.php');
+            $objambito = new Cambito();
+            $resultado = $objambito->cInsertarAmbitos($nombreAmbitos);
+            echo $resultado;
+        } elseif($sw==1) {
+            echo "Campo vacio";
+        }else{
+            echo "Error en la Inserción";
+        }
     ?>
 </body>
 </html>
